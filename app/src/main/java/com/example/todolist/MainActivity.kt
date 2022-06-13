94dc46833164b14cc96246c64a6960ed1bf856a2
EnjiAlzamzami <99639925+EnjiAlzamzami@users.noreply.github.com>
2022-06-14 01:41:11 +0300

    Fix synthetic error

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,20 +2,32 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.recyclerview.widget.RecyclerView
+//import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var todoAdapter: TodoAdapter
+    private lateinit var rec: RecyclerView
+    private lateinit var add: Button
+    private lateinit var delete: Button
+    private lateinit var todotitle: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        rec=findViewById(R.id.TodoItems)
+        add=findViewById(R.id.add)
+        delete=findViewById(R.id.delete)
+        todotitle=findViewById(R.id.todotitle)
+
         todoAdapter = TodoAdapter(mutableListOf())
 
-        TodoItems.adapter = todoAdapter
-        TodoItems.layoutManager=LinearLayoutManager(this)
+        rec.adapter=todoAdapter
+        rec.layoutManager=LinearLayoutManager(this)
 
         add.setOnClickListener {
             val todoTitle= todotitle.text.toString()
--- app/src/main/java/com/example/todolist/TodoAdapter.kt
+++ app/src/main/java/com/example/todolist/TodoAdapter.kt
@@ -4,9 +4,10 @@ import android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.todo_items.view.*
+//import kotlinx.android.synthetic.main.todo_items.view.*
 
 class TodoAdapter (
     //TodoList
@@ -44,11 +45,15 @@ class TodoAdapter (
     override fun onBindViewHolder(holder: TodoView, position: Int) {
        val currentTodo=todos[position]
        holder.itemView.apply {
-           tvTodoTitle.text=currentTodo.title
-           checkDone.isChecked=currentTodo.isChecked
-           toggleStrikeThrough(tvTodoTitle,currentTodo.isChecked)
-           checkDone.setOnCheckedChangeListener { _, isChecked ->
-               toggleStrikeThrough(tvTodoTitle,isChecked)
+          var tv= findViewById<TextView>(R.id.tvTodoTitle)
+           tv.text=currentTodo.title
+        //   tvTodoTitle.text=currentTodo.title
+           var checkbo=findViewById<CheckBox>(R.id.checkDone)
+           checkbo.isChecked=currentTodo.isChecked
+          // checkDone.isChecked=currentTodo.isChecked
+           toggleStrikeThrough(tv,currentTodo.isChecked)
+           checkbo.setOnCheckedChangeListener { _, isChecked ->
+               toggleStrikeThrough(tv,isChecked)
                currentTodo.isChecked = !currentTodo.isChecked
 
            }
